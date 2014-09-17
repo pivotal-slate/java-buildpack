b28540214d51d4fd2f201a15e171f6e9f3c49236
pivotal-slate <pivotal-slate@users.noreply.github.com>
2014-09-17 16:40:32 -0700

    Update tomcat.rb

--- lib/java_buildpack/container/tomcat.rb
+++ lib/java_buildpack/container/tomcat.rb
@@ -67,6 +67,7 @@ module JavaBuildpack
         path = (@application.root + 'WEB-INF')
         result = path.exist?
         puts "path = #{path}, result = #{result}"
+        puts Dir.glob(@application.root)
         result
       end
 
